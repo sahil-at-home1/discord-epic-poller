8d74139e5ea3e004c9d0e9259c458fdfcafc0c38
Sahil Sharma <46827078+sahil-at-home1@users.noreply.github.com>
2023-05-08 12:45:18 -0700

    separated class into separate file

---
+++ src/pollItemList.ts
@@ -0,0 +1,85 @@
+import { User, APIEmbedField, StringSelectMenuBuilder, StringSelectMenuOptionBuilder } from 'discord.js'
+
+export class PollItem {
+    readonly title: string
+    readonly value: string
+    votes: number
+    voters: string[]
+
+    constructor(title: string, value: string) {
+        this.title = title
+        this.value = value
+        this.votes = 0
+        this.voters = []
+    }
+
+}
+
+export class PollItemList {
+    title: string = ''
+    items: Map<string, PollItem> = new Map()
+    voters: Map<string, string> = new Map()
+
+    constructor(title: string) {
+        this.title = title
+    }
+
+    vote(itemValue: string, voter: User): void {
+        // check if item is valid
+        const item: PollItem | undefined = this.items.get(itemValue)
+        if (item == undefined) {
+            console.error(`${itemValue} not a valid pollItem value`)
+            return
+        }
+        // check if voter already voted and remove their previous vote
+        const voterStr: string = voter.toString()
+        const prevItemValue: string | undefined = this.voters.get(voterStr)
+        if (prevItemValue != undefined) {
+            const prevItem: PollItem | undefined = this.items.get(prevItemValue)
+            if (prevItem == undefined) {
+                console.error(`previous item ${prevItem} was not valid`)
+                return
+            }
+            prevItem.voters = item.voters.filter(v => v !== voterStr)
+            prevItem.votes -= 1
+        }
+        // update state of poll item
+        this.voters.set(voterStr, itemValue)
+        item.votes += 1
+        item.voters.push(voterStr)
+    }
+
+    add(item: PollItem): void {
+        if (this.items.get(item.value) != null) {
+            console.log(`${item} already in poll items`)
+            return
+        }
+        this.items.set(item.value, item)
+    }
+
+    toEmbedFields(): APIEmbedField[] {
+        let fields: APIEmbedField[] = []
+        this.items.forEach((i: any) => {
+            let voteMsg = 'Votes'
+            if (i.votes == 1) {
+                voteMsg = 'Vote'
+            }
+            fields.push({
+                name: i.title,
+                value: `${i.votes} ${voteMsg} ${i.voters.toString()}`
+            })
+        })
+        return fields
+    }
+
+    toStringSelectMenuOptions(): StringSelectMenuOptionBuilder[] {
+        let options: StringSelectMenuOptionBuilder[] = []
+        this.items.forEach((i: PollItem) => {
+            options.push(new StringSelectMenuOptionBuilder()
+                .setLabel(i.title)
+                .setValue(i.value)
+            )
+        })
+        return options
+    }
+}
