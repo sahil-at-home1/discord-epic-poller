badfe36ca226976908f1ac21abcef5439bd3aef9
Sahil Sharma <46827078+sahil-at-home1@users.noreply.github.com>
2023-05-07 21:44:08 -0700

    refactored to poll item classes

--- src/commands/poll.ts
+++ src/commands/poll.ts
@@ -1,16 +1,71 @@
-import { ActionRowBuilder, bold, Embed, CommandInteraction, ComponentType, InteractionResponse, SlashCommandBuilder, SlashCommandStringOption, StringSelectMenuBuilder, StringSelectMenuComponent, StringSelectMenuInteraction, StringSelectMenuOptionBuilder, TextInputBuilder, EmbedBuilder, Collection } from "discord.js"
+import { ActionRowBuilder, bold, User, CommandInteraction, ComponentType, InteractionResponse, SlashCommandBuilder, SlashCommandStringOption, StringSelectMenuBuilder, StringSelectMenuComponent, StringSelectMenuInteraction, StringSelectMenuOptionBuilder, TextInputBuilder, EmbedBuilder, Collection, APIEmbedField } from "discord.js"
 
 class PollItem {
-    title: string
-    value: string
+    readonly title: string
+    readonly value: string
     votes: number
     voters: string[]
 
-    constructor(title: string, value: string, votes: number, voters: string[]) {
+    constructor(title: string, value: string) {
         this.title = title
         this.value = value
-        this.votes = votes
-        this.voters = voters
+        this.votes = 0
+        this.voters = []
+    }
+
+}
+
+class PollItemList {
+    items: Map<string, PollItem> = new Map()
+    voters: Map<string, any> = new Map()
+
+    vote(itemValue: string, voter: User): void {
+        if (this.voters.get(voter.toString()) != undefined) {
+            console.log(`${voter} has already voted`)
+            return
+        }
+        this.voters.set(voter.toString(), {})
+        const item: PollItem | undefined = this.items.get(itemValue)
+        if (item == undefined) {
+            console.error(`${itemValue} not a valid pollItem value`)
+            return
+        }
+        item.votes += 1
+        item.voters.push(voter.toString())
+    }
+
+    add(item: PollItem): void {
+        if (this.items.get(item.value) != null) {
+            console.log(`${item} already in poll items`)
+            return
+        }
+        this.items.set(item.value, item)
+    }
+
+    toEmbedFields(): APIEmbedField[] {
+        let fields: APIEmbedField[] = []
+        this.items.forEach((i: any) => {
+            let voteMsg = 'Votes'
+            if (i.votes == 1) {
+                voteMsg = 'Vote'
+            }
+            fields.push({
+                name: i.title,
+                value: `${i.votes} ${voteMsg} ${i.voters.toString()}`
+            })
+        })
+        return fields
+    }
+
+    toStringSelectMenuOptions(): StringSelectMenuOptionBuilder[] {
+        let options: StringSelectMenuOptionBuilder[] = []
+        this.items.forEach((i: PollItem) => {
+            options.push(new StringSelectMenuOptionBuilder()
+                .setLabel(i.title)
+                .setValue(i.value)
+            )
+        })
+        return options
     }
 }
 
@@ -28,44 +83,29 @@ export const Poll = {
     execute: async (interaction: CommandInteraction) => {
         // immediate reply
         await interaction.deferReply({
-            // ephemeral: true
+            // ephemeral: true,
         })
-        // await interaction.reply(`your title was ${title}`)
-        const title: string = bold(interaction.options.get('title')?.value as string ?? 'Untitled Poll')
-        // const title_row = new ActionRowBuilder<Text>
 
-        let pollItems: PollItem[] = [
-            { title: 'Option 1', value: '1', votes: 0, voters: [] },
-            { title: 'Option 2', value: '2', votes: 0, voters: [] },
-            { title: 'Option 3', value: '3', votes: 0, voters: [] },
-        ]
+        const pollItems: PollItemList = new PollItemList()
+        pollItems.add(new PollItem('Option 1', '0'))
+        pollItems.add(new PollItem('Option 2', '1'))
+        pollItems.add(new PollItem('Option 3', '2'))
 
         // creating the poll selections
         const poll = new StringSelectMenuBuilder()
             .setCustomId('poll')
             .setPlaceholder('Choose from the following...')
-        pollItems.forEach((pi: PollItem) => {
-            poll.addOptions(
-                new StringSelectMenuOptionBuilder()
-                    .setLabel(pi.title)
-                    .setValue(pi.value)
-            )
-        })
+            .setOptions(pollItems.toStringSelectMenuOptions())
         const select_row = new ActionRowBuilder<StringSelectMenuBuilder>()
             .addComponents(poll)
 
-
         // create the embed to show the results
+        const title: string = bold(interaction.options.get('title')?.value as string ?? 'Untitled Poll')
         const results = new EmbedBuilder()
             .setColor(0x0099FF)
             .setTitle(`Poll: ${title}`)
             .setTimestamp()
-        pollItems.forEach((pi: any) => {
-            results.addFields({
-                name: pi.title,
-                value: `${pi.votes} Vote(s) ${pi.voters.toString()}`
-            })
-        })
+            .addFields(pollItems.toEmbedFields())
 
         // send the message
         const response = await interaction.followUp({
@@ -82,18 +122,13 @@ export const Poll = {
 
         // send update
         collector.on('collect', async i => {
-            const selection: number = Number(i.values[0])
+            const selection: string = i.values[0]
+            console.log(selection)
+            console.log(pollItems)
             // create new results embed
-            pollItems[selection].votes += 1
-            pollItems[selection].voters.push(i.user.toString())
+            pollItems.vote(selection, i.user)
             const newResults = EmbedBuilder.from(response.embeds[0])
-            newResults.setFields([])
-            pollItems.forEach((pi: PollItem) => {
-                newResults.addFields({
-                    name: pi.title,
-                    value: `${pi.votes} Vote(s) ${pi.voters.toString()}`
-                })
-            })
+            newResults.setFields(pollItems.toEmbedFields())
             await interaction.editReply({ embeds: [newResults] })
         })
     }
